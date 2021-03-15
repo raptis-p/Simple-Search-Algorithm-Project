b171cb2a5acd6b76e553c2d70213b3b42c7579f9
raptis-p <raptis.panagiotis96@gmail.com>
2021-03-15 12:43:03 +0200

    Minor changes in Graph class

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,5 +1,12 @@
 package graph;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Graph {
 
+	
+	private Map<Node, List <Node>> adjNodes= new HashMap<Node, List <Node>>();
+	
 }
